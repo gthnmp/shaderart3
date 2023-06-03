51e13ee893fb4885a8dcbaf2f06383ee7850e562
Gathan Mahesa <gthn217@gmail.com>
2023-06-03 22:50:43 +0700

    Initial Commit

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -3,12 +3,11 @@ uniform vec2 iDeviceRatio;
 uniform float iTime;
 
 // cosine based palette, 4 vec3 params, from : https://iquilezles.org/articles/palettes/
-vec3 palette( float t )
-{
-	vec3 a = vec3(0.5);
-	vec3 b = vec3(0.5);
-	vec3 c = vec3(1.0, 1.0, 0.5);
-	vec3 d = vec3(0.80, 0.90, 0.30);
+vec3 palette( float t ) {
+    vec3 a = vec3(0.5, 0.5, 0.5);
+    vec3 b = vec3(0.5, 0.5, 0.5);
+    vec3 c = vec3(1.0, 1.0, 1.0);
+    vec3 d = vec3(0.263,0.416,0.557);
 
     return a + b*cos( 6.28318*(c*t+d) );
 }
@@ -21,9 +20,9 @@ void main(){
 	for (float i = 0.0; i < 4.0; i++){
 		uv = fract(uv * 1.5) - 0.5;
 
-		vec3 color = palette(length(uv0) + iTime * 0.4);
+		float radius = length(uv) * exp(-length(uv0));
+		vec3 color = palette(length(uv0) + i * 0.4 + iTime * 0.4);
 
-		float radius = length(uv) + i * 0.1 * exp(-length(uv0));
 		radius = sin(radius * 8.0 + iTime) / 8.0;
 		radius = abs(radius);
 		radius = pow(0.01 / radius, 1.2);	
